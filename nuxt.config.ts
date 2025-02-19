002a21505627a784ee52175282f9c8fe2368c136
Chiu Wah Li <chiuwah@gmail.com>
2025-02-19 11:24:04 +0100

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,25 @@
+import Aura from '@primevue/themes/aura'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  css: ['~/assets/tailwind.css', '~/assets/styles.scss'],
+  modules: ['@primevue/nuxt-module'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  primevue: {
+    options: {
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: '.app-dark',
+        },
+      },
+    },
+  },
+})
